d83b01e4a6d39e60c44a01ace76399646aa80ddf
UnterKatze <fabianmeyer1997@gmail.com>
2020-04-20 00:47:03 +0200

    Started integrating the code for the display

--- WeatherStationV2_Sketch/WeatherStationV2_Sketch.ino
+++ WeatherStationV2_Sketch/WeatherStationV2_Sketch.ino
@@ -24,7 +24,7 @@ unsigned long set2020_2 = 1603587600; // 25.10.2020 03:00 UTC+2
 unsigned long set2021_1 = 1616893200; // 28.03.2021 02:00 UTC+1
 unsigned long set2021_2 = 1635642000; // 31.10.2021 03:00 UTC+2
 
-const char *ssid     = "Kwik-E-Mart";
+const char *ssid = "Kwik-E-Mart";
 const char *password = "9589089603281286";
 
 WiFiUDP ntpUDP;
@@ -32,10 +32,14 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
 unsigned status;
 
+String data;
+
 // ************************************ F U N C T I O N S ******************************************** //
 
-void printSensorData() {
-  if (status) {
+void printSensorData()
+{
+  if (status)
+  {
     Serial.print("Temperature = ");
     Serial.print(bme.readTemperature(), 1);
     Serial.println(" °C");
@@ -54,17 +58,22 @@ void printSensorData() {
   }
 }
 
-void printTime() {
+void printTime()
+{
   int hour, minute, second;
   unsigned long epoch_time;
-  if ((WiFi.status() == WL_CONNECTED)) {
+  if ((WiFi.status() == WL_CONNECTED))
+  {
     hour = timeClient.getHours();
     minute = timeClient.getMinutes();
     second = timeClient.getSeconds();
     epoch_time = timeClient.getEpochTime();
-    if ((epoch_time >= set2020_1) && (epoch_time <= set2020_2)) {
+    if ((epoch_time >= set2020_1) && (epoch_time <= set2020_2))
+    {
       hour++;
-    } else {
+    }
+    else
+    {
     }
     Serial.println(epoch_time);
     Serial.print(hour);
@@ -76,40 +85,77 @@ void printTime() {
   }
 }
 
+void printToDisplay()
+{
+  Serial.print(data);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+  Serial.write(0xFF);
+}
+
 // **************************************** S E T U P ************************************************ //
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  while (!Serial) {
-    delay(1);                                                             // time to get serial running
+  while (!Serial)
+  {
+    delay(1); // time to get serial running
   }
-  Serial.println("\nSerial working");
+
+  data = "t0.pco=65535";
+  printToDisplay(); // Controller Starting
+
+  data = "t3.pco=7648";
+  printToDisplay(); // positive response
+
+  data = "t1.pco=65535";
+  printToDisplay(); // Starting Serial
+
+  data = "t4.pco=7648";
+  SprintToDisplay(); // positive response
 
   WiFi.begin(ssid, password);
   Serial.print("Connecting to Internet ");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    data = "t2.pco=65535";
+    printToDisplay(); // Connecting to Internet
     delay(100);
     int timer_100ms = 0;
     timer_100ms++;
-    if (timer_100ms >= 100) {
-      Serial.println("\nNo Internet Connection");
+    if (timer_100ms >= 100)
+    {
+      data = "t8.pco=63488";
+      printToDisplay(); // negative response
       break;
     }
   }
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("\nInternet Connection Established");
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    data = "t7.pco=7648";
+    printToDisplay(); // positive response
     timeClient.begin();
   }
 
   status = bme.begin();
+  data = "t9.pco=65535";
+  printToDisplay(); // Configuring Sensor
   delay(100);
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor");
+  if (!status)
+  {
+    data = "t11.pco=63488";
+    printToDisplay(); // negative response
+  }
+  else
+  {
+    data = "t10.pco=7648";
+    printToDisplay(); // positive response
   }
 }
 
 // **************************************** L O O P ************************************************** //
-void loop() {
+void loop()
+{
   timeupdate = timeClient.update();
 
   printSensorData();
