2ae883a546878b6e603e09a598a8a4086b83b535
UnterKatze <fabianmeyer1997@gmail.com>
2020-05-18 22:16:11 +0200

    try to fix under 10 degree bug

--- WeatherStationV2_Sketch/WeatherStationV2_Sketch.ino
+++ WeatherStationV2_Sketch/WeatherStationV2_Sketch.ino
@@ -295,14 +295,14 @@ void setDisplayPicture()
       setCorrectTextboxBackgroundPicture(6);
       return;
     }
-    if ((WiFi.status() == WL_CONNECTED) && (temperature < 0.0))
+    if ((WiFi.status() == WL_CONNECTED) && (temperature <= 0.0))
     {
       data = "page1.pic=9";
       printToDisplay(); // display cold picture with wifi on
       setCorrectTextboxBackgroundPicture(9);
       return;
     }
-    if ((WiFi.status() != WL_CONNECTED) && (temperature < 0.0))
+    if ((WiFi.status() != WL_CONNECTED) && (temperature <= 0.0))
     {
       data = "page1.pic=8";
       printToDisplay(); // display cold picture with wifi off
@@ -310,14 +310,14 @@ void setDisplayPicture()
       return;
     }
 
-    if ((WiFi.status() == WL_CONNECTED) && (temperature < 10.0))
+    if ((WiFi.status() == WL_CONNECTED) && (temperature <= 10.0))
     {
       data = "page1.pic=5";
       printToDisplay(); // display coldish picture with wifi on
       setCorrectTextboxBackgroundPicture(5);
       return;
     }
-    if ((WiFi.status() != WL_CONNECTED) && (temperature < 10.0))
+    if ((WiFi.status() != WL_CONNECTED) && (temperature <= 10.0))
     {
       data = "page1.pic=4";
       printToDisplay(); // display coldish picture with wifi off
@@ -325,14 +325,14 @@ void setDisplayPicture()
       return;
     }
 
-    if ((WiFi.status() == WL_CONNECTED) && (temperature < 20.0))
+    if ((WiFi.status() == WL_CONNECTED) && (temperature <= 20.0))
     {
       data = "page1.pic=1";
       printToDisplay(); // display warmer picture with wifi on
       setCorrectTextboxBackgroundPicture(1);
       return;
     }
-    if ((WiFi.status() != WL_CONNECTED) && (temperature < 20.0))
+    if ((WiFi.status() != WL_CONNECTED) && (temperature <= 20.0))
     {
       data = "page1.pic=0";
       printToDisplay(); // display warmer picture with wifi off
@@ -340,14 +340,14 @@ void setDisplayPicture()
       return;
     }
 
-    if ((WiFi.status() == WL_CONNECTED) && (temperature < 30.0))
+    if ((WiFi.status() == WL_CONNECTED) && (temperature <= 30.0))
     {
       data = "page1.pic=3";
       printToDisplay(); // display warm picture with wifi on
       setCorrectTextboxBackgroundPicture(3);
       return;
     }
-    if ((WiFi.status() != WL_CONNECTED) && (temperature < 30.0))
+    if ((WiFi.status() != WL_CONNECTED) && (temperature <= 30.0))
     {
       data = "page1.pic=2";
       printToDisplay(); // display warm picture with wifi off
@@ -374,7 +374,7 @@ void setDisplayPicture()
 
 void setDisplayTextColor()
 {
-  if (temperature < 13.0)
+  if (temperature <= 13.0)
   {
     data = "n2.pco=63488"; // red temp
     printToDisplay();
@@ -383,7 +383,7 @@ void setDisplayTextColor()
   }
   else
   {
-    if (temperature < 19.0)
+    if (temperature <= 19.0)
     {
       data = "n2.pco=65504"; // yellow temp
       printToDisplay();
